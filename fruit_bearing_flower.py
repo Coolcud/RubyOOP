19b97960835042ebedb73ba77cc2211f49e3d697
Sage Choi <coolcuds@gmail.com>
2023-04-05 14:19:13 -0700

    Create class with constructor and instance methods.

--- fruit_bearing_flower.py
+++ fruit_bearing_flower.py
@@ -1,10 +1,12 @@
 from flower import Flower
+from fruit import Fruit
 
 class FruitBearingFlower(Flower):
-    def __init__(self, flower_color, fruit_type, fruit_count):
+    def __init__(self, flower_color, fruit_type, fruit_count, fruit):
         super().__init__(color = flower_color, is_blooming = True, produces_fruit = True)
         self.fruit_type = fruit_type
         self.fruit_count = fruit_count
+        self.fruit = fruit
 
     def smell(self):
         print("This flower smells nice.")
@@ -14,3 +16,6 @@ class FruitBearingFlower(Flower):
         self.fruit_count -= 1
         print(f"We currently have this much fruit: {self.fruit_count}")
 
+    def produce_fruit(self):
+        fruit = Fruit("good", "rainbow")
+        self.fruit = fruit
