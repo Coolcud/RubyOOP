c286aee5462784ab25ca9d65fdd824a4bbb13e8e
Sage Choi <coolcuds@gmail.com>
2023-04-05 14:21:01 -0700

    Created child object of FruitBearingFlower.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from flower import Flower
 from fruit_bearing_flower import FruitBearingFlower
+from fruit import Fruit
 
-apple_flower = FruitBearingFlower("white", "apple", 1)
-# print(apple_flower.color)
-apple_flower.smell()
-# regular_flower = Flower("blue", produces_fruit = True)
-# print(f"This flower is blooming: {regular_flower.is_blooming}")
-# print(f"This flower produces fruit: {regular_flower.produces_fruit}")
-# regular_flower.smell()
-# apple_flower.neighbor(regular_flower)
-# apple_flower.pick_fruit()
+blackberry = Fruit("sweet", "black")
+# print(blackberry.color, blackberry.taste)
+# print(blackberry.is_ripe())
+
+print()
+cherry_blossom = FruitBearingFlower("pink", "cherry", 3, blackberry)
+# print(cherry_blossom.color)
+print(cherry_blossom.fruit)
+
+cherry_blossom.produce_fruit()
+cherry_blossom.produce_fruit()
+cherry_blossom.produce_fruit()
+print(cherry_blossom.fruit)
+
+# apple_flower = FruitBearingFlower("white", "apple", 1)
+# # print(apple_flower.color)
+# apple_flower.smell()
+# # regular_flower = Flower("blue", produces_fruit = True)
+# # print(f"This flower is blooming: {regular_flower.is_blooming}")
+# # print(f"This flower produces fruit: {regular_flower.produces_fruit}")
+# # regular_flower.smell()
+# # apple_flower.neighbor(regular_flower)
+# # apple_flower.pick_fruit()
